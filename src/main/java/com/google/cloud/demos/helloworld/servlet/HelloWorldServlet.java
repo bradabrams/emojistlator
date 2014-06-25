b919a59dabde6fbb1fe429891a97e64355641bb5
Chris Ramsdale <cramsdale@google.com>
2014-06-25 14:06:02 -0700

    initial commit

---
+++ src/main/java/com/google/cloud/demos/helloworld/servlet/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package com.google.cloud.demos.helloworld.servlet;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+              throws IOException {
+
+        resp.setContentType("text/plain");
+        resp.getWriter().println("testing testing 78910");
+    }
+}
