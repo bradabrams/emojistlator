010fc616c92f2144aecbe8b44e27dc488622c0d4
Brad Abrams <bma@google.com>
2014-10-10 18:15:09 -0700

    adding a load testing script

---
+++ loadscript.sh
@@ -0,0 +1,27 @@
+while :
+do
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+curl http://localhost:8080/?deleteall
+curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
+curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+
+done
+
+
+
