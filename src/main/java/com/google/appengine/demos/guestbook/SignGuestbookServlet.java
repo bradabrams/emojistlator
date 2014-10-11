bc3e902dfb6d80b888d7b037e4b43d4be85e4d9a
Brad Abrams <bma@google.com>
2014-10-10 18:47:50 -0700

    added random delay

--- src/main/java/com/google/appengine/demos/guestbook/SignGuestbookServlet.java
+++ src/main/java/com/google/appengine/demos/guestbook/SignGuestbookServlet.java
@@ -33,6 +33,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.logging.Logger;
+import java.util.Random ; 
 
 public class SignGuestbookServlet extends HttpServlet {
 
@@ -42,6 +43,12 @@ public class SignGuestbookServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
+
+    if (randInt (0,100) < 50) {
+       try {
+          Thread.sleep(5000);  //5000 milliseconds is five second.
+       } catch(InterruptedException ex) { } 
+    }
 
     UserService userService = UserServiceFactory.getUserService();
     User user = userService.getCurrentUser();
@@ -115,5 +122,28 @@ public class SignGuestbookServlet extends HttpServlet {
     }
     return (String[]) l.toArray(new String[0]);
   }
+
+  /**
+ * Returns a pseudo-random number between min and max, inclusive.
+ * The difference between min and max can be at most
+ * <code>Integer.MAX_VALUE - 1</code>.
+ *
+ * @param min Minimum value
+ * @param max Maximum value.  Must be greater than min.
+ * @return Integer between min and max, inclusive.
+ * @see java.util.Random#nextInt(int)
+ */
+public static int randInt(int min, int max) {
+
+    // NOTE: Usually this should be a field rather than a method
+    // variable so that it is not re-seeded every call.
+    Random rand = new Random();
+
+    // nextInt is normally exclusive of the top value,
+    // so add 1 to make it inclusive
+    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+    return randomNum;
+}
 
 }
