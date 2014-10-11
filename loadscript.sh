ba1d8d7ceaf0fc4ca03ec445a82fb203167116d9
Brad Abrams <bma@google.com>
2014-10-10 18:28:09 -0700

    a few more tweeks

--- loadscript.sh
+++ loadscript.sh
@@ -1,25 +1,25 @@
+path="http://debuggerdemo.appspot.com"
+path="http://localhost:8080"
+
+echo $path/sign
+curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+
+
 while :
 do
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
-curl http://localhost:8080/?deleteall
-curl -X POST --data "content=kiss&guestbookName=default" http://localhost:8080/sign
-curl -X POST --data "content=kiss dog&guestbookName=default" http://localhost:8080/sign
+ curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss dog&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss dog&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss dog&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss dog&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss dog&guestbookName=default" $path/sign
+ curl $path?deleteall
+ curl -X POST --data "content=kiss&guestbookName=default" $path/sign
+ curl -X POST --data "content=kiss dog&guestbookName=default" $path/sign
 
 done
 
